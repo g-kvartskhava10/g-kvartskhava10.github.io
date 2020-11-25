446bb1d370112eb2c019d4a442c921b895a2eeca
g-kvartskhava10 <g.kvartskhava.art19@gipa.ge>
2020-11-26 00:13:23 +0400

    new task

---
+++ 4/list dropdown.js
@@ -0,0 +1,48 @@
+var dropdownButtons = document.getElementsByClassName("dropdown-btn");
+
+
+for (var i=0; i<dropdownButtons.length; i++){
+    dropdownButtons[i].addEventListener("click", function (){
+        this.parentElement.getElementsByClassName("dropdown-list")[0].style.display= "flex";
+    })
+}
+
+var textCols = document.getElementsByClassName("col");
+
+var dropdownItems = document.getElementsByClassName("dropdown-item");
+
+for(var i=0; i<dropdownItems.length; i++)
+{
+    dropdownItems[i].addEventListener("click", function (){
+        for (var j=0; j<textCols.length; j++){
+            textCols[j].style.display = "none";
+        }
+        this.parentElement.parentElement.getElementsByClassName("dropdown-btn")[0].innerHTML = this.innerHTML;
+        this.parentElement.style.display = "none";
+        document.getElementById(this.dataset.target).style.display = "flex";
+
+    });
+}
+
+var albums = [
+    Orchid, Morningrise, Delivarance, Sorceress, Ghost, Damnation, Watershed
+]
+
+
+    var changed = false
+    do {
+        changed = false
+        for (var i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                var b = a[i];
+                a[1] = a[i + 1];
+                a [i + 1] = b;
+                changed = true;
+            }
+        }
+    } while (changed);
+
+
+
+
+
